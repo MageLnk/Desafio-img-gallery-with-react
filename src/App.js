d15c9386dc1235aa86844a9e7439e90d012c5aa3
MageLink <mage.link.zhezard@gmail.com>
2022-08-16 21:02:53 -0400

    La clase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import Producto from "./components/Producto";
+
 const App = () => {
-  return <div>Ola k ase</div>;
+  return (
+    <>
+      <Producto infoToShow={"Televisor"} priceToShow={"9.999"} />
+      <Producto infoToShow={"Radio"} priceToShow={"5.550"} />
+    </>
+  );
 };
 
 export default App;
