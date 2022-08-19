86f3382f07a9e5baed2abeb29f5dd92bd5f4fe81
MageLink <mage.link.zhezard@gmail.com>
2022-08-18 21:47:30 -0400

    Layout ready

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import Producto from "./components/Producto";
+// Components
+import { HeaderContent } from "./components";
+// Design
+import { Row, Col } from "antd";
 
 const App = () => {
   return (
     <>
-      <Producto infoToShow={"Televisor"} priceToShow={"9.999"} />
-      <Producto infoToShow={"Radio"} priceToShow={"5.550"} />
+      <Row className="header-row-container" justify="space-between" align="middle">
+        <HeaderContent />
+      </Row>
+      <Row className="content-row-container" justify="center">
+        <Row>
+          <Col span={24}>
+            <span>Content o k ase</span>
+          </Col>
+        </Row>
+      </Row>
+      <Row className="footer-row-container" justify="end" align="middle">
+        <Row>
+          <Col span={24}>
+            <span>Footer o k ase</span>
+          </Col>
+        </Row>
+      </Row>
     </>
   );
 };
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,22 @@
+// Style
+import { Row, Col } from "antd";
+// App
+
+const HeaderContent = () => {
+  return (
+    <>
+      <Row>
+        <Col span={24}>
+          <span>Logo</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <span>Final o k ase</span>
+        </Col>
+      </Row>
+    </>
+  );
+};
+
+export default HeaderContent;
